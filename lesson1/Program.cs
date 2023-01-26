f5dc02f78b7ca2f871d7d65473fe0c0815a4caca
Serge Kripkovsky <122611492+Custodiant@users.noreply.github.com>
2023-01-26 23:48:47 +0700

    Добавлено_описание_некоторых_функций_C#

---
+++ lesson1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Запомним важные команды:
+
+Console.WriteLine("---------");
+Console.WriteLine("Для того, чтобы создать среду для C#, необходимо в терминате ввести 'dotnet new console'");
+//Создание среды C#
+
+Console.WriteLine("---------");
+Console.WriteLine("Любой ваш текст");
+Console.WriteLine("Будет отображаться с новой строки");
+Console.WriteLine("Если в каждой из строк введете 'Console.WriteLine ,нужный текст, в скобках'");
+//Что бы Данные отображались в терминале, введите сочетание Console.WriteLine("Бла-Бла");
+
+Console.WriteLine("---------");
+Console.Write("Любой ваш текст,");
+Console.Write("можно продолжить без переноса строки,");
+Console.WriteLine("если прописать 'Console.Write ,нужный текст, в скобках'");
+//Чтобы данные отображались в терминале, введите сочетание Console.Write("Бла-Бла");
+
+Console.WriteLine("---------");
+Console.WriteLine("Чтобы запросить у пользователя ввод данных, необходимо прописать 'Console.ReadLine ,нужный текст, в скобках");
+Console.WriteLine("Введите ваши данные, после знака:");
+Console.ReadLine();
+
