900cb4d54b66b5a2918380bfbe605690cebfdf65
Serge Kripkovsky <122611492+Custodiant@users.noreply.github.com>
2023-01-28 17:37:58 +0700

    добавлена_5_задача

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Введите трёх значное число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num > 999 || num < 100)
+{
+    Console.WriteLine("Введите число от 100 до 999 включительно");
+}
+else
+{
+    int val = num / 10;
+    int res = num - val * 10;
+    Console.WriteLine("Последняя цифра вашего числа: " + res);
+}
